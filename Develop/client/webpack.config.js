30b6beaa4bd054ef156c40f631aa7fa9014892d8
clvalada <clvalada@gmail.com>
2023-12-05 14:35:04 -0500

    updated webpack.config.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,27 +3,69 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // HTML Webpack Plugin
+       
+       new HtmlWebpackPlugin({
+        template: './src/index.html', // Path to your HTML template
+        filename: 'index.html',
+        chunks: ['main'],
+      }),
+
+      // Webpack PWA Manifest Plugin
+      new WebpackPwaManifest({
+        name: 'Your PWA Name',
+        short_name: 'Your PWA',
+        description: 'Your PWA Description',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+
+      // Workbox Plugin
+      new InjectManifest({
+        swSrc: './src/sw.js', // path to your service worker file
+        swDest: 'service-worker.js',
+      }),
     ],
 
     module: {
       rules: [
-        
+        // CSS Loader
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+
+        // Babel Loader
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
