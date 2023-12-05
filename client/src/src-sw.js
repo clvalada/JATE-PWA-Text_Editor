c9d0946424358e3e0095b10496a52c27b420a1f2
clvalada <clvalada@gmail.com>
2023-12-05 15:31:40 -0500

    created src-sw file

---
+++ client/src/src-sw.js
@@ -0,0 +1,50 @@
+// src-sw.js
+
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate, NetworkFirst } from 'workbox-strategies';
+
+// Precache and route all assets in the build folder
+precacheAndRoute(self.__WB_MANIFEST);
+
+// Cache the Google Fonts stylesheets with a StaleWhileRevalidate strategy
+registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+
+// Cache the Google Fonts webfonts with a CacheFirst strategy
+registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
+// Cache images with a NetworkFirst strategy
+registerRoute(
+  /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+  new NetworkFirst({
+    cacheName: 'images',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+// Ensure that your service worker file is being loaded
+console.log('Service Worker is loaded!');
